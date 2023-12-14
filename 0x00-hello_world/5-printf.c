52628b023d5b31e234929e9182a6521eba1ff0a1
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-14 14:48:55 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main  - entry function
+ *Return : return 0 after success
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
