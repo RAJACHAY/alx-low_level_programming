9c6329baaed83287783850b232538bfa05c71296
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-14 14:24:20 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/*
+ *
+ * main - principal function
+ * Return: return 0 after execute
+ */
+int main(void);
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
