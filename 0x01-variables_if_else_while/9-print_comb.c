7faece960f6739a2d63805323655d5fea66fb84d
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 10:45:45 -0800

    ESPACE

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- *  main - Print char
- *  Return: retun afet success
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int a;
+	int num;
 
-	for (a = '0'; a < '9'; a++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar((a % 10) + '0');
-		if (a == 9)
+		putchar((num % 10) + '0');
+		if (num == 9)
 			continue;
-
+		
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('9');
+
+	putchar('\n');
+
 	return (0);
 }
