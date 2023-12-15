9091dfb2a0233968cc838efa3938d09f3dee2819
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-14 16:11:37 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry function
+ * Return: 0 if executed
+ */
+
+int main(void)
+{
+	printf("Size of a char: sifeof(char) byte(s)");
+	print("Size of an int: 4 byte(s)");
+	print("Size of a long int: sizeof(long int) byte(s)");
+	print("Size of a long long int: sizeof(long long byte(s)");
+	print("Size of a float: sizeof(float) byte(s)");
+	return (0);
+}
