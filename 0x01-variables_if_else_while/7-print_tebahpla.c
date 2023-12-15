a2e03722ffb641463493b5844dcf8b879a181fa7
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 09:55:42 -0800

    REVERSE ALPH

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+O#include <stdio.h>
+
+/**
+ *  main - Print char
+ *  Return: retun afet success
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'a'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
