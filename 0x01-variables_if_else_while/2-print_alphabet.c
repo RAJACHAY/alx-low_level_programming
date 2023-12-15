f60e8f63d8f9a282d9aea4ee9d37a1f8904ee3a2
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 03:09:12 -0800

    lower alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - Print char
+ *  Return: retun afet success
+ */
+
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                putchar(c);
+        }
+        return (0);
+}
