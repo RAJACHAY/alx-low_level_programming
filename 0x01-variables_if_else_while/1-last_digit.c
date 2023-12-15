8779945949fa203e865cb2bbf35d42ed48fecdac
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 03:03:57 -0800

    lower alphabet

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - Print char
+ *  Return: retun afet success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = "a"; c <= "z"; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
+
