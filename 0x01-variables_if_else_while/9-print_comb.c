ec67a07317d3c9d3fe458208aa690480a6e2d6c6
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 10:29:11 -0800

    ESPACE

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Print char
+ *  Return: retun afet success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
