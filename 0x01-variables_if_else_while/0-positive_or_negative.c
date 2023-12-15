c2cca4ee169921fc4ce8b7816b310b611e0c2689
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 02:42:11 -0800

    what sign

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,7 @@
 #include <time.h>
 
 /**
- *
  * main - Function detemine the sign
- *
  * Return: return 0 after finish
  */
 
@@ -15,12 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive", n);
 	}
-	else {
-		if (n<0)
+	else
+	{
+		if (n < 0)
 		{
 			printf("%d is negative", n);
 		}
