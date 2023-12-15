8c24de7cb2112e023005949cd53e7affc9c3aa0f
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-15 09:40:36 -0800

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - Print char
+ *  Return: retun afet success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
