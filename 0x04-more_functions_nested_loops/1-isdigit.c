ea170ae1a3a44d4090df19f8d6362f425bab7965
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2023-12-21 13:40:16 -0800

    up3

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-*mul - function that multipliestwo numbers
-*@a: first number
-*@b: second number
-*Return: returns result
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
 */
 
-int mul(int a, int b)
+int _isdigit(int c)
 {
-return (a * b);
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
 }
