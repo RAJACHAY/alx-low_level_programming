993298438008aff84273ebe170fa8e6f36ad1f96
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-01-02 15:29:28 -0800

    2 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@s: string
+ * Return: Always 0.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+
+	return ();
+}
