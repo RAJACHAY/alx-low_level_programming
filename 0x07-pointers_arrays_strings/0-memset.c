168b406ad7b1486624a5a60b0e637c8cc2853699
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-01-08 10:16:58 -0800

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: pointer to fill
+ * @b: constant byte
+ * @n: number to fill
+ *
+ * Return: return pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
