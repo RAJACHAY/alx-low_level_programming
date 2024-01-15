9f97610c4b52a92dfc2421df4355237709592993
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-01-15 15:56:42 -0800

    up

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * str_concat - get ends of input and add together for size
@@ -10,28 +11,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char  *arr;
-	int i, l, j, k;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == NULL || s2 == NULL)
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
+
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
 
-	while (s1[i] != '\n')
-		i++;
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
 
-	while (s2[i] != '\n')
-		j++;
-
-	k = i + j;
-	arr = malloc(sizeof(char) * (k + 1));
-
-	for (l = 0; l < i; l++)
-	{
-		arr[l] = s1[i];
-	}
-	for (i = 0; i < j; i++, l++)
-	{
-		arr[l] = s2[i];
-	}
-	return (arr);
+	return (result);
 }
