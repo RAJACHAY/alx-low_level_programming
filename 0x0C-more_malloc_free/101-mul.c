837820a2b8ad39a0544395be343696a89044c44f
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-01-18 17:24:48 -0800

    e

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,21 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
+#define ERR_MSG "Error"
 
 /**
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
  * Return: The length of the string.
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 int find_len(char *str)
+int is_digit(char *s)
 {
 	int len = 0;
+	int i = 0;
 
 	while (*str++)
 		len++;
@@ -78,12 +80,17 @@ int get_digit(char c)
 	int digit = c - '0';
 
 	if (digit < 0 || digit > 9)
+	while (s[i])
 	{
 		printf("Error\n");
 		exit(98);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
 
 	return (digit);
+	return (1);
 }
 
 /**
@@ -92,11 +99,15 @@ int get_digit(char c)
  * @mult: The string of numbers.
  * @digit: The single digit.
  * @zeroes: The necessary number of leading zeroes.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
  * Description: If mult contains a non-digit, the function
  *              exits with a status value of 98.
+ * Return: the length of the string
  */
 void get_prod(char *prod, char *mult, int digit, int zeroes)
+int _strlen(char *s)
 {
 	int mult_len, num, tens = 0;
 
@@ -108,6 +119,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 		*prod = 'x';
 		prod++;
 	}
+	int i = 0;
 
 	prod--;
 
@@ -118,6 +130,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	}
 
 	for (; mult_len >= 0; mult_len--, mult--, prod--)
+	while (s[i] != '\0')
 	{
 		if (*mult < '0' || *mult > '9')
 		{
@@ -129,10 +142,12 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 		num += tens;
 		*prod = (num % 10) + '0';
 		tens = num / 10;
+		i++;
 	}
 
 	if (tens)
 		*prod = (tens % 10) + '0';
+	return (i);
 }
 
 /**
@@ -140,8 +155,10 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
  * @final_prod: The buffer storing the running final product.
  * @next_prod: The next product to be added.
  * @next_len: The length of next_prod.
+ * errors - handles errors for main
  */
 void add_nums(char *final_prod, char *next_prod, int next_len)
+void errors(void)
 {
 	int num, tens = 0;
 
@@ -175,16 +192,22 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 
 	if (tens)
 		*final_prod = (tens % 10) + '0';
+	printf("Error\n");
+	exit(98);
 }
 
 /**
  * main - Multiplies two positive numbers.
  * @argv: The number of arguments passed to the program.
  * @argc: An array of pointers to the arguments.
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Description: If the number of arguments is incorrect or one number
  *              contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
@@ -192,6 +215,21 @@ int main(int argc, char *argv[])
 	int size, index, digit, zeroes = 0;
 
 	if (argc != 3)
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		printf("Error\n");
 		exit(98);
@@ -216,16 +254,36 @@ int main(int argc, char *argv[])
 		digit = get_digit(*(argv[2] + index));
 		get_prod(next_prod, argv[1], digit, zeroes++);
 		add_nums(final_prod, next_prod, size - 1);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
 	for (index = 0; final_prod[index]; index++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (final_prod[index] != 'x')
 			putchar(final_prod[index]);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
 	putchar('\n');
 
 	free(next_prod);
 	free(final_prod);
 
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
