8f74d1bf05b3f28e2ab1c2843dc5e853003a0b34
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-07-16 06:26:27 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		printf("Comparing value: [%d]\n", array[i]);
 		if (value == array[i])
